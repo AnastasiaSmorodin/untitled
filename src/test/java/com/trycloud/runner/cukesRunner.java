c573835e8e1b05675f731aef938ec5f609c7f86c
AnastasiaSmorodin <anastasiasmorodin@gmail.com>
2022-04-26 09:22:37 -0500

    123

--- src/test/java/com/trycloud/runner/cukesRunner.java
+++ src/test/java/com/trycloud/runner/cukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         plugin = {
-                //"pretty",
+                "pretty",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
